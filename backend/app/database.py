6acdb238d147388064696d39aa1d4f5e29db9faa
shkityr.k <shkityr.k@yandex.ru>
2024-10-16 03:20:05 +0300

    i'm sry, hah:)

--- backend/app/database.py
+++ backend/app/database.py
@@ -13,7 +13,6 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'konstantin')
 
 DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
-print("хуета")
 try:
 
     engine = create_engine(DATABASE_URL)
